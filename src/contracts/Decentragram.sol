a42ec66eb9019cd67e51646c413c717740c74bf1
Sajal Tiwari <sajaltiwari003@gmail.com>
2021-05-05 12:36:12 +0530

    Added function store post

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,7 +2,36 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   // Code goes here...
-  string public name = "SocialBlock";
+  string public name = "Decentragram";
+  
+  
+
+  //Store Images
+  mapping (uint=>Image) public images;
+  struct Image{
+    uint id;
+    string hash;
+    string description;
+    string tipAmmount;
+    address payable author;
+  }
+
+
+  //Create Images
+
+  function uploadImage() public {
+    images[1] = Image(1,'abc777','Hello World','0',address(0x0));    
+    
+  }
+
+
+
+
+
+
+
+  
+  //Tip Images
 
 
 
